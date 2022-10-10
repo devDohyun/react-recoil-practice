b7601926a0f455d5c876a08c4153015779fa2be5
devDohyun <dohyun10101@gmail.com>
2022-10-11 07:27:47 +0900

    Fix(model): resolve item typing

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,3 +1,3 @@
 export const withLatency = <T extends Array<any>, U>(fn: (...args: T) => U, timeout: number = 1500) => {
-  return (...args: T) => new Promise((res) => setTimeout(() => res(fn(...args)), timeout))
+  return (...args: T) => new Promise<U>((res) => setTimeout(() => res(fn(...args)), timeout))
 }
