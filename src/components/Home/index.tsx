6075feac755b429c5ad3a93a019186df14627776
devDohyun <dohyun10101@gmail.com>
2022-10-11 08:22:08 +0900

    Feat(product): Skeleton UI 추가

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,30 @@
 import { Suspense } from 'react'
+import { SkeletonSquare } from '../common/Skeletons'
+import Stack from '../common/Stack'
 import TextTop from '../common/TextTop'
 import ProductList from './ProductList'
+
+const ProductListFallback = () => {
+  return (
+    <>
+      {Array.from({ length: 6 }).map((_, idx) => (
+        <Stack key={idx} marginBottom="40px">
+          <SkeletonSquare width="75px" height="75px" marginRight="10px" />
+          <div>
+            <SkeletonSquare width="125px" height="22px" marginBottom="5px" />
+            <SkeletonSquare width="225px" height="18px" />
+          </div>
+        </Stack>
+      ))}
+    </>
+  )
+}
 
 const Home = () => {
   return (
     <>
       <TextTop typography="h4">쇼핑하기</TextTop>
-      <Suspense>
+      <Suspense fallback={<ProductListFallback />}>
         <ProductList />
       </Suspense>
     </>
