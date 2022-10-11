a0c4f87904e7e83b636b6d793ca02bd86d2e42f8
devDohyun <dohyun10101@gmail.com>
2022-10-11 22:47:54 +0900

    Feat: productDetailQuery selector 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,20 @@
-import { selector } from 'recoil'
-import { getProducts } from '../models'
+import { selector, selectorFamily } from 'recoil'
+import { getProductById, getProducts } from '../models'
+import { TProduct } from '../types'
 
 export const productsQuery = selector({
   key: 'productsQuery',
   get: async () => {
     const response = await getProducts()
+
+    return response
+  },
+})
+
+export const productDetailQuery = selectorFamily({
+  key: 'productDetailQuery',
+  get: (productId: TProduct['id']) => async () => {
+    const response = await getProductById(productId)
 
     return response
   },
