612816989a8e41f2bf0d78c4246a2701057718ee
devDohyun <dohyun10101@gmail.com>
2022-10-11 23:11:33 +0900

    Feat(Stack): height prop 추가

--- src/components/common/Stack/index.tsx
+++ src/components/common/Stack/index.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled'
 
-type TStack = Partial<Pick<CSSStyleDeclaration, 'justifyContent' | 'alignContent' | 'marginBottom'>>
+type TStack = Partial<Pick<CSSStyleDeclaration, 'justifyContent' | 'alignContent' | 'marginBottom' | 'height'>>
 
 const Stack = styled.div<TStack>`
   display: flex;
   justify-content: ${({ justifyContent }) => justifyContent ?? 'center'};
   align-items: ${({ alignContent }) => alignContent ?? 'center'};
+
+  height: ${({ height }) => height ?? 'auto'};
 
   margin-bottom: ${({ marginBottom }) => marginBottom ?? '0'};
 `
