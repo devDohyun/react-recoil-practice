cb19844b7b7269096e0c5ff3d304ab4f724e1465
devDohyun <dohyun10101@gmail.com>
2022-10-11 22:17:58 +0900

    Feat: model handler 추가

--- src/models/handlers.ts
+++ src/models/handlers.ts
@@ -1,6 +1,10 @@
+import { TProduct } from '../types'
 import { products } from './data'
 import { withLatency } from './utils'
 
 export const getProducts = withLatency(() => {
   return [...products]
+})
+export const getProductById = withLatency((id: TProduct['id']) => {
+  return products.find((item) => item.id === id)
 })
