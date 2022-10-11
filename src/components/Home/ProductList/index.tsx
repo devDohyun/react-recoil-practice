85889af204f4009ba605e2a415a6fb9e1b85f341
devDohyun <dohyun10101@gmail.com>
2022-10-11 23:34:58 +0900

    Refactor(Home): Sekeleton UI 함께두기

--- src/components/Home/ProductList/index.tsx
+++ src/components/Home/ProductList/index.tsx
@@ -1,20 +1,42 @@
+import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 import { productsQuery } from '../../../store'
+import { SkeletonSquare } from '../../common/Skeletons'
+import Stack from '../../common/Stack'
 import ProductListItem from './Item'
 
 const ProductList = () => {
   const products = useRecoilValue(productsQuery)
 
   return (
-    <>
+    <StyledContainer>
       {products.map((item) => (
         <Link key={item.id} to={`/product/${item.id}`}>
           <ProductListItem item={item} />
         </Link>
       ))}
-    </>
+    </StyledContainer>
   )
 }
 
+const ProductListFallback = () => (
+  <StyledContainer>
+    {Array.from({ length: 6 }).map((_, idx) => (
+      <Stack key={idx} marginBottom="40px" justifyContent="flex-start">
+        <SkeletonSquare width="75px" height="75px" marginRight="10px" />
+        <div>
+          <SkeletonSquare width="125px" height="22px" marginBottom="5px" />
+          <SkeletonSquare width="225px" height="18px" />
+        </div>
+      </Stack>
+    ))}
+  </StyledContainer>
+)
+
+const StyledContainer = styled.div`
+  padding: 0 20px;
+`
+
+export { ProductListFallback }
 export default ProductList
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,24 +1,6 @@
 import { Suspense } from 'react'
-import { SkeletonSquare } from '../common/Skeletons'
-import Stack from '../common/Stack'
 import TextTop from '../common/TextTop'
-import ProductList from './ProductList'
-
-const ProductListFallback = () => {
-  return (
-    <>
-      {Array.from({ length: 6 }).map((_, idx) => (
-        <Stack key={idx} marginBottom="40px">
-          <SkeletonSquare width="75px" height="75px" marginRight="10px" />
-          <div>
-            <SkeletonSquare width="125px" height="22px" marginBottom="5px" />
-            <SkeletonSquare width="225px" height="18px" />
-          </div>
-        </Stack>
-      ))}
-    </>
-  )
-}
+import ProductList, { ProductListFallback } from './ProductList'
 
 const Home = () => {
   return (
