2a08a182d2ec4f2321ccec806cb2baabdd9b2244
devDohyun <dohyun10101@gmail.com>
2022-10-11 22:52:20 +0900

    Feat(utils): formatNumber 함수 추가

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,3 +1,7 @@
 export function getDateString(timestamp: number) {
   return new Date(timestamp).toLocaleDateString('ko')
 }
+
+export function formatNumber(number: number) {
+  return number.toLocaleString('ko')
+}
