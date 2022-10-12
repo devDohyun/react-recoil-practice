b49c1e5901e947fe327ff7fedf8423bd14497330
devDohyun <dohyun10101@gmail.com>
2022-10-12 09:15:23 +0900

    Feat(model): 결제 완료 여부 확인 API 추가

--- src/models/handlers.ts
+++ src/models/handlers.ts
@@ -8,3 +8,16 @@ export const getProducts = withLatency(() => {
 export const getProductById = withLatency((id: TProduct['id']) => {
   return products.find((item) => item.id === id)
 })
+
+const paymentCache: { [key: string]: number } = {}
+export const checkPaymentDone = withLatency((orderId: string) => {
+  paymentCache[orderId] = (paymentCache[orderId] || 0) + 1
+  const isDone = paymentCache[orderId] > 5
+
+  if (isDone) delete paymentCache[orderId]
+
+  return {
+    orderId,
+    isDone,
+  }
+}, 1000)
