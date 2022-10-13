82294e73065a7044d5e881dcb4feb9827b822af5
devDohyun <dohyun10101@gmail.com>
2022-10-13 22:24:14 +0900

    Feat: page load 시 상단 스크롤 하기

--- src/pages/Product/Detail.tsx
+++ src/pages/Product/Detail.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import ProductDetail from '../../components/ProductDetail'
 
 const PageProductDetail = () => {
   const [searchParams] = useSearchParams()
   const id = searchParams.get('id')
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   if (!id) return <>잘못된 접근입니다.</>
 
