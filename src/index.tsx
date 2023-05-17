cc357584fd56231e974aa2fd9b4b82a3e7594b2f
devDohyun <dohyun10101@gmail.com>
2023-05-18 08:36:29 +0900

    Feat(msw): start on app init

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { server } from './server/browser'
+
+server.start()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
     <App />
