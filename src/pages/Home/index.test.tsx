98270d61732e31acae9da768481c54e72683ab06
devDohyun <dohyun10101@gmail.com>
2023-05-20 14:38:40 +0900

    Fix(testing): use msw

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -1,31 +1,20 @@
 import { screen } from '@testing-library/react'
-import Home from '../../components/Home'
+
 import { renderWithBaseComponents } from '../../core/testUtils'
-import { TProduct } from '../../types'
-import * as remotes from '../../core/http/remotes'
+import Home from '../../components/Home'
+import { server } from '../../server/node'
 
-const mockProducts: TProduct[] = [
-  {
-    id: 'P12082',
-    img: '/product-2.png',
-    name: 'Apple watch',
-    price: 599000,
-    content: `Atoms는 React의 로컬 컴포넌트의 상태 대신 사용할 수 있다.`,
-    shipping: 1665538745200,
-    stock: 81,
-  },
-]
-
-jest.mock('../../core/http/remotes', () => ({ getProducts: jest.fn(() => Promise.resolve(mockProducts)) }))
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'bypass' })
+})
+afterAll(() => {
+  server.close()
+})
 
 describe('<Home />', () => {
   test('상품 목록이 잘 노출되나요?', async () => {
-    const spyOnGetProducts = jest.spyOn(remotes, 'getProducts')
-
     renderWithBaseComponents(<Home />, { route: '/' })
 
     expect(await screen.findByText(/쇼핑하기/)).toBeInTheDocument()
-
-    expect(spyOnGetProducts).toHaveBeenCalled()
   })
 })
---
+++ src/server/node.ts
@@ -0,0 +1,4 @@
+import { setupServer } from 'msw/node'
+import { getHandlers } from './handlers'
+
+export const server = setupServer(...getHandlers())
